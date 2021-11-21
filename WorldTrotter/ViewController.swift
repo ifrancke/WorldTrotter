0c0c858ee68037c9bd4e9630ed32188ca02dde7d
Isabelle Francke <ifrancke@uvm.edu>
2021-11-21 13:54:21 -0500

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Mac Admin on 11/21/21.
+//  Copyright © 2021 Mac Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
